c6d693cf15ae757e7b0376f2c501198a7f053722
yunieom <yunzoo0915@gmail.com>
2024-11-01 15:26:38 +0900

    #10 feat: 백엔드 로그인 기능 구현 - 유저 아이디와 비밀번호를 받아 jwt 반환(userId, role, iat, exp) - accessToken 15분

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginReqDto } from './dto/req/login.req.dto';
 import { SignUpReqDto } from './dto/req/signup.req.dto';
 import { SignUpResDto } from './dto/res/signup.res.dto';
 import { UsersService } from './users.service';
@@ -13,5 +14,11 @@ export class UsersController {
 	@ApiResponse({ type: SignUpResDto })
 	async signUp(@Body() user: SignUpReqDto): Promise<SignUpResDto> {
 		return this.usersService.signUp(user);
+	}
+
+	// 로그인
+	@Post('login')
+	async login(@Body() user: LoginReqDto): Promise<{ accessToken: string }> {
+		return this.usersService.login(user);
 	}
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+	imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), AuthModule],
 	controllers: [UsersController],
 	providers: [UsersService],
-	exports: [UsersService]
+	exports: [UsersService],
 })
 export class UsersModule {}
