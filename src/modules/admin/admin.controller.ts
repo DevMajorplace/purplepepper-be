8c24cdae8195cfc6354d622b7f7d94d53204d115
seokhyunp <psh2@majorplace.net>
2024-11-18 14:32:54 +0900

    feat: 사용자 권한 기반 접근 기능 수정
    - `RoleGuard`를 새로 만들어서 `AuthGuard`에 있던 권한 처리 기능을 분리
    - 사용자 권한 설정을 `Class` 단위로도 설정할 수 있게 수정
    - `UserRoles` Decorator의 입력 값 형식을 수정함

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -4,6 +4,7 @@ import { UserStatusResDto } from '../admin/dto/res/user.status.res.dto';
 import { UserStatusUpdateResDto } from '../admin/dto/res/user.status.update.res.dto';
 import { UserStatusQueryDto } from '../admin/dto/user-status-query.dto';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
 import { UserRoles } from '../auth/types/role.decorator';
 import { Role } from '../auth/types/role.enum';
 import { AdminService } from './admin.service';
@@ -17,14 +18,14 @@ import { ClientDetailResDto } from './dto/res/client.detail.res.dto';
 import { ClientListResDto } from './dto/res/client.list.res.dto';
 
 @ApiTags('Admin')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
+@UserRoles(Role.Admin)
 @Controller('admin')
 export class AdminController {
 	constructor(private readonly adminService: AdminService) {}
 
 	// 가입 대기/거절 회원 조회
 	@Get('status')
-	@UserRoles([Role.Admin])
 	async findUsersByStatus(
 		@Query('page') page: number,
 		@Query('pageSize') pageSize: number,
@@ -41,7 +42,6 @@ export class AdminController {
 
 	// 가입 승인(단일, 다중 사용자)
 	@Patch('approve')
-	@UserRoles([Role.Admin])
 	async approveUsers(
 		@Body() userIds: string[],
 	): Promise<{ updatedUsers: UserStatusUpdateResDto[]; missingUserIds: string[] }> {
@@ -50,7 +50,6 @@ export class AdminController {
 
 	// 가입 거절(단일, 다중 사용자)
 	@Patch('decline')
-	@UserRoles([Role.Admin])
 	async declineUsers(
 		@Body() userIds: string[],
 	): Promise<{ updatedUsers: UserStatusUpdateResDto[]; missingUserIds: string[] }> {
@@ -59,7 +58,6 @@ export class AdminController {
 
 	// 가입된 광고주 목록 조회
 	@Get('clients')
-	@UserRoles([Role.Admin])
 	async getAllClients(
 		@Query('page') page: number,
 		@Query('pageSize') pageSize: number,
@@ -76,14 +74,12 @@ export class AdminController {
 
 	// 단일 광고주 상세 조회
 	@Get('client')
-	@UserRoles([Role.Admin])
 	async getClientDetail(@Query('userId') userId: string): Promise<ClientDetailResDto> {
 		return this.adminService.getClientDetail(userId);
 	}
 
 	// 단일 광고주 상세 정보 변경
 	@Patch('client')
-	@UserRoles([Role.Admin])
 	@UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
 	async updateClientDetail(
 		@Query('userId') userId: string,
--- src/modules/boards/boards.controller.ts
+++ src/modules/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
 import { UserRoles } from '../auth/types/role.decorator';
 import { Role } from '../auth/types/role.enum';
 import { BoardsService } from './boards.service';
@@ -10,12 +11,13 @@ import { BoardListResDto } from './dto/res/board.list.res.dto';
 import { Board } from './schemas/board.schema';
 
 @ApiTags('Boards')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('boards')
 export class BoardsController {
 	constructor(private readonly boardService: BoardsService) {}
 
 	@Get()
+	@UserRoles(Role.Admin, Role.Agency, Role.Client)
 	@ApiResponse({ type: BoardListResDto })
 	async getBoardsList(
 		@Query('page') page: number,
@@ -28,27 +30,28 @@ export class BoardsController {
 	}
 
 	@Get(':id')
+	@UserRoles(Role.Admin, Role.Agency, Role.Client)
 	@ApiResponse({ type: Board })
 	async getBoard(@Param('id') id: string): Promise<BoardDetailResDto> {
 		return this.boardService.getBoardById(id);
 	}
 
 	@Post()
-	@UserRoles([Role.Admin])
+	@UserRoles(Role.Admin)
 	@ApiResponse({ type: Board })
 	async createBoard(@Body() board: BoardReqDto): Promise<Board> {
 		return this.boardService.createBoard(board);
 	}
 
 	@Put(':id')
-	@UserRoles([Role.Admin])
+	@UserRoles(Role.Admin)
 	@ApiResponse({ type: BoardDetailResDto })
 	async updateBoard(@Param('id') id: string, @Body() board: BoardReqDto): Promise<BoardDetailResDto> {
 		return this.boardService.updateBoard(id, board);
 	}
 
 	@Delete(':id')
-	@UserRoles([Role.Admin])
+	@UserRoles(Role.Admin)
 	@ApiResponse({ type: BoardDetailResDto })
 	async deleteBoard(@Param('id') id: string): Promise<BoardDetailResDto> {
 		return this.boardService.deleteBoard(id);
