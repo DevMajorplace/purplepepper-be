4ce882755f0a6c7a0ec4df172b5d6b4ff67640ca
seokhyunp <psh2@majorplace.net>
2024-12-02 13:02:25 +0900

    feat: 현재 로그인한 사용자의 활성화 여부를 체크하는 기능 추가

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/db/schema/user.schema';
 import { AuthService } from './auth.service';
 
 @Module({
 	imports: [
+		MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
 		JwtModule.registerAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -1,12 +1,12 @@
-import { BadRequestException, CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { ERROR_MESSAGE_NO_TOKEN } from '../../../common/constants/error-messages';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { ERROR_MESSAGE_INVALID_USER, ERROR_MESSAGE_NO_TOKEN } from '../../../common/constants/error-messages';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 	constructor(private authService: AuthService) {}
 
-	canActivate(context: ExecutionContext): boolean {
+	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const request = context.switchToHttp().getRequest();
 		const token = request.cookies?.access_token;
 
@@ -15,7 +15,8 @@ export class AuthGuard implements CanActivate {
 		}
 
 		// 요청 객체에 사용자 정보를 추가하여 이후 로직에서 사용 가능
-		const payload = this.authService.verifyToken(token, 'access');
+		const payload = await this.authService.verifyTokenAsync(token);
+		if (!payload) throw new ForbiddenException(ERROR_MESSAGE_INVALID_USER);
 		request.user = payload;
 
 		return true;
